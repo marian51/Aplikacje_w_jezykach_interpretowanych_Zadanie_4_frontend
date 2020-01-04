2fafd8bfeb89ae8e200cbf430c1db5a83c7248e9
Mariusz <mariusz.jozwiak@int.pl>
2020-01-04 20:41:27 +0100

    Usługi obsługi produktu

---
+++ src/services/ProductDataService.js
@@ -0,0 +1,37 @@
+/**
+ * Usługa będzie używała axios do obsługi HTTP
+ */
+
+ import http from "../http-common";
+
+ class ProductDataService {
+     getAll() {
+         return http.get("/products");
+     }
+
+     get(id) {
+         return http.get(`/products/${id}`);
+     }
+
+     create(data) {
+         return http.post("/products", data);
+     }
+
+     update(id, data) {
+         return http.put(`/products/${id}`, data);
+     }
+
+     delete(id) {
+         return http.delete(`/products/${id}`);
+     }
+
+     deleteAll() {
+         return http.delete(`/products`);
+     }
+
+     findByName(name) {
+         return http.get(`/products?name=${name}`);
+     }
+ }
+
+ export default new ProductDataService();
