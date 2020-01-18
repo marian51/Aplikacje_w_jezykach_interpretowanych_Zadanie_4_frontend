72c376bb3deca24236e1da2dd913b406ad0a92a5
Mariusz <mariusz.jozwiak@int.pl>
2020-01-18 16:25:38 +0100

    Dodanie Vue-Select

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import vSelect from 'vue-select'
 import App from './App.vue'
 import router from "./router"
 
 Vue.config.productionTip = false
 
+Vue.component('v-select', vSelect)
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'vue-select/dist/vue-select.css';
 
 new Vue({
   router,
