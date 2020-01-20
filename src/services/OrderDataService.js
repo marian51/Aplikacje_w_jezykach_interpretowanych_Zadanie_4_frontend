2365dab5b882632da5893e639f0e198d87f5e2a4
Mariusz <mariusz.jozwiak@int.pl>
2020-01-20 23:17:38 +0100

    Routy wyciągania wszystkich zamówień i powiązań zamówienie-produkt

--- src/services/OrderDataService.js
+++ src/services/OrderDataService.js
@@ -4,6 +4,14 @@ class OrderDataService {
     create(data) {
         return http.post("/orders", data);
     }
+    
+    getAll() {
+        return http.get("/orders");
+    }
+
+    createEntity(data) {
+        return http.post("/orderproducts", data);
+    }
 }
 
 export default new OrderDataService();
