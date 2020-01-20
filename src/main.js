33e2ff4cb8de55dd602f0738cce2ed5494d10b5a
Mariusz <mariusz.jozwiak@int.pl>
2020-01-20 01:08:39 +0100

    Doinstalowanie vuelidate i vue-select

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import vSelect from 'vue-select'
 import App from './App.vue'
 import router from "./router"
+import Vuelidate from "vuelidate"
 
 Vue.config.productionTip = false
 
 Vue.component('v-select', vSelect)
+
+Vue.use(Vuelidate);
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'vue-select/dist/vue-select.css';
